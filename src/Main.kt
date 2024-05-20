5ae742c0219a82212995aa0ba9073a1c22c19587
Donovan <Donovanxcr@gmail.com>
2024-05-20 15:53:14 -0600

    mi primer commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//print sirve para imprimir en consola
+    print("hola mundo")
+
+}
